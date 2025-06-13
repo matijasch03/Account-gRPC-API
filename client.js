e660c40dc7e442ddd406e5ce6b08bcc413a40e83
Matija Šeremet <matija.sch03@gmail.com>
2025-06-13 12:56:45 +0200

    Add input comand line and validation

--- client.js
+++ client.js
@@ -6,11 +6,14 @@ const userProto = grpc.loadPackageDefinition(packageDefinition).user;
 
 const client = new userProto.UserService('localhost:50051', grpc.credentials.createInsecure());
 
-client.CreateUser({ name: 'Petar', email: 'petar@gmail.com'}, (err, response) => {
+// these two commas reserved for the first two words 'node cliennt.js' in the command line
+const [, , name, email] = process.argv;
+
+client.CreateUser({name, email}, (err, response) => {
   if (!err) {
     console.log('Successfully created user with id', response.id);
   } else {
-    console.error('Error:', err);
+    console.error('Error:', err.message);
   }
 });
 
@@ -18,6 +21,6 @@ client.GetUsers({}, (err, response) => {
   if (!err) {
     console.log('User list:', response.users);
   } else {
-    console.error('Error:', err);
+    console.error('Error:', err.message);
   }
 });
--- server.js
+++ server.js
@@ -7,9 +7,29 @@ const users = [];
 let idCounter = 0;
 
 function createUser(call, callback) {
-    call.request.id = idCounter++;
-    users.push(call.request);
-    callback(null, {id: call.request.id});
+  errorMessage = returnErrorMessage(call.request.name, call.request.email);
+  if (errorMessage){
+    const err = {
+      code: grpc.status.INVALID_ARGUMENT,
+      message: errorMessage
+    };
+    return callback(err, {});
+  }
+
+  call.request.id = idCounter++;
+  users.push(call.request);
+  callback(null, {id: call.request.id});
+}
+
+function returnErrorMessage(name, email) {
+  if (!name || !email)
+    return 'Incorrect format error\nCorrect one: node client.js name email';
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email))
+    return 'Incorrect email format\nCorrect one: email@gmail.com';
+
+  return '';
 }
 
 function getUsers (_, callback) {
